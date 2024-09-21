91f0f78cac9be528b03a05361c88cb55367026c9
riyaas <syedriyaasS@gmail.com>
2024-09-21 14:52:09 +0530

    mobile_responsive_with_animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 // sticky-navbar
-document.addEventListener('DOMContentLoaded', function() {
-    var navbar = document.querySelector('.navbar');
-    var descSection = document.querySelector('.banner-btns');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var navbar = document.querySelector('.navbar');
+//     var descSection = document.querySelector('.banner-btns');
 
-    window.addEventListener('scroll', function() {
-        var rect = descSection.getBoundingClientRect();
-        if (rect.top <= 0) {
-            navbar.classList.add('sticky');
-        } else {
-            navbar.classList.remove('sticky');
-        }
-    });
-});
+//     window.addEventListener('scroll', function() {
+//         var rect = descSection.getBoundingClientRect();
+//         if (rect.top <= 0) {
+//             navbar.classList.add('sticky');
+//         } else {
+//             navbar.classList.remove('sticky');
+//         }
+//     });
+// });
 
 
 
